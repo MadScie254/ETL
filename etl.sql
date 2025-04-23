164c08795b6c9202b6a22106310e04bc691a0138
Daniel Wanjala <134260642+MadScie254@users.noreply.github.com>
2025-04-23 21:32:28 +0300

    updates

--- etl.sql
+++ etl.sql
@@ -4,3 +4,6 @@ ALTER USER etl_user WITH PASSWORD '12345';
 
 -- Grant access to the database
 GRANT ALL PRIVILEGES ON DATABASE etl_project TO etl_user;
+GRANT USAGE ON SCHEMA public TO etl_user;
+GRANT CREATE ON SCHEMA public TO etl_user;
+GRANT ALL ON SCHEMA public TO etl_user;
